68417b0bdf7078ba6c323b9b943043820ff23c3d
ThallesSorrilha <thallessorrilha@gmail.com>
2026-02-25 21:50:36 -0300

    feature(GameManager): Separando responsabilidades de input, update e draw. Criando controle de FPS

--- src/GameManager/GameManager.cpp
+++ src/GameManager/GameManager.cpp
@@ -1,17 +1,22 @@
 #include "GameManager.h"
 #include <iostream>
 
+Uint32 frameStart;
+Uint32 frameTime;
+
 bool GameManager::init()
 {
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         std::cerr << "Video failed\n";
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
     window = SDL_CreateWindow("Jogo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
     if (!window)
     {
         std::cerr << "Window failed\n";
+        std::cout << SDL_GetError() << "\n";
         return false;
     }
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -21,20 +26,17 @@ bool GameManager::init()
 
 void GameManager::run()
 {
-    SDL_Event e;
     while (running)
     {
-        while (SDL_PollEvent(&e))
+        frameStart = SDL_GetTicks();
+        GameManager::handleInput();
+        GameManager::update();
+        GameManager::draw();
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < DELAY_TIME)
         {
-            if (e.type == SDL_QUIT)
-                running = false;
+            SDL_Delay((int)(DELAY_TIME - frameTime));
         }
-        SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
-        SDL_RenderClear(renderer);
-        SDL_Rect r{350, 250, 100, 100};
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_RenderFillRect(renderer, &r);
-        SDL_RenderPresent(renderer);
     }
 }
 
@@ -43,4 +45,28 @@ void GameManager::shutdown()
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
+}
+
+void GameManager::handleInput()
+{
+    SDL_Event e;
+    while (SDL_PollEvent(&e))
+    {
+        if (e.type == SDL_QUIT)
+            running = false;
+    }
+}
+
+void GameManager::update()
+{
+}
+
+void GameManager::draw()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 100, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_Rect r{350, 250, 100, 100};
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &r);
+    SDL_RenderPresent(renderer);
 }
--- src/GameManager/GameManager.h
+++ src/GameManager/GameManager.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <SDL2/SDL.h>
+#include <chrono>
 
 class GameManager
 {
@@ -9,7 +10,14 @@ public:
     void shutdown();
 
 private:
+    void handleInput();
+    void update();
+    void draw();
+
     SDL_Window *window = nullptr;
     SDL_Renderer *renderer = nullptr;
     bool running = false;
+
+    const int FPS = 60;
+    const Uint32 DELAY_TIME = 1000.0f / FPS;
 };
