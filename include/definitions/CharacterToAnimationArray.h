3e5ae62fdf8515acc358cd09102e0d545311b404
ThallesSorrilha <thallessorrilha@gmail.com>
2026-07-26 22:27:27 -0300

    fix(CharacterToAnimation): corrigindo animações faltantes no array

--- include/definitions/CharacterToAnimationArray.h
+++ include/definitions/CharacterToAnimationArray.h
@@ -4,8 +4,10 @@
 
 #include "../enums/AnimationID.h"
 
-constexpr std::array<AnimationID, 3> CharacterToAnimationArray = {
-    AnimationID::Character_IdleDown,  // 0
-    AnimationID::Character_WalkDown,  // 1
-    AnimationID::Character_AttackDown // 2
+constexpr std::array<AnimationID, 5> CharacterToAnimationArray = {
+    AnimationID::Character_IdleDown,   // Idle
+    AnimationID::Character_WalkDown,   // Walking
+    AnimationID::Character_AttackDown, // Attacking
+    AnimationID::Character_IdleDown,   // Suffering_Damage
+    AnimationID::Character_IdleDown    // Dead
 };
