5645683be96c1201e4807e4f67c7a05a60a87f3c
ThallesSorrilha <thallessorrilha@gmail.com>
2026-07-28 21:02:55 -0300

    feature(AudioArray, enums/Audio): adicionando mapeamento para áudios

---
+++ include/definitions/AudioArray.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string_view>
+#include <array>
+
+constexpr std::array<std::string_view, 9> AudioArray = {
+    "assets/audio/jingles/GameOver.wav",             // 0
+    "assets/audio/jingles/Secret3.wav",              // 1
+    "assets/audio/jingles/Success3.wav",             // 2
+    "assets/audio/musics/Village.ogg",               // 3
+    "assets/audio/sounds/bonus/Coin3.wav",           // 4
+    "assets/audio/sounds/hits_e_impact/Hit3.wav",    // 5
+    "assets/audio/sounds/hits_e_impact/Impact2.wav", // 6
+    "assets/audio/sounds/woosh_e_slash/Sword2.wav",  // 7
+    "assets/audio/sounds/woosh_e_slash/Whoosh.wav"   // 8
+};
---
+++ include/enums/Audio.h
@@ -0,0 +1,14 @@
+#pragma once
+
+enum class Audio
+{
+    Sound_GameOver,
+    Sound_Secret,
+    Sound_Success,
+    Music_Village,
+    Sound_Coin,
+    Sound_Hit,
+    Sound_Impact,
+    Sound_Sword,
+    Sound_Whoosh
+};
